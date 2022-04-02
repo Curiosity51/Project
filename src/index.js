c86f6ddca80d0f22bdf6a8754d146cccdbd9a0f1
Curiosity51 <k.durbas@gmail.com>
2022-04-02 14:46:27 +0200

    Basic setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './scss/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
